db6dbe491a51ec5d80cfc8f6c570434b54a860b4
wozoo <wazabidev@gmail.com>
2018-08-24 06:41:50 +0700

    g
    g

---
+++ NoobSDK.framework/Headers/NoobSDK.h
@@ -0,0 +1,19 @@
+//
+//  NoobSDK.h
+//  NoobSDK
+//
+//  Created by XD on 8/23/18.
+//  Copyright © 2018 nab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NoobSDK.
+FOUNDATION_EXPORT double NoobSDKVersionNumber;
+
+//! Project version string for NoobSDK.
+FOUNDATION_EXPORT const unsigned char NoobSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NoobSDK/PublicHeader.h>
+
+
